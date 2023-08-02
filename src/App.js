6066758e4583e01092b19804ea67f4628d2924ff
me-nk007 <nishantkarn007@gmail.com>
2023-08-02 21:02:57 +0545

    Redux Project Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './Navbar';
+import Shop from './Shop';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+    <>
+    <Navbar/>
+    <div className="container">
+      <Shop/>
     </div>
+    </>
   );
 }
 
---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Shop = () => {
+  return (
+    <div>
+        <h2 className='my-3'>Buy Adidas Shoes Rs.50</h2>
+      <button className="btn btn-primary mx-2">-</button>
+      <strong>Add to cart</strong>
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  );
+}
+
+export default Shop;
